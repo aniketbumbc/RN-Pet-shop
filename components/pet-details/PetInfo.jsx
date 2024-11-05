b410ae53c1be9fb5521246fc3edfccc3de7c9cae
aniket <aniket.umbc@gmail.com>
2024-11-05 22:27:37 +0530

    icon added

--- components/pet-details/PetInfo.jsx
+++ components/pet-details/PetInfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text, Image } from 'react-native';
 import Colors from '../../constants/Colors';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function PetInfo({ pet }) {
   return (
@@ -16,6 +17,10 @@ export default function PetInfo({ pet }) {
       <View
         style={{
           padding: 20,
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
         }}
       >
         <View>
@@ -32,6 +37,10 @@ export default function PetInfo({ pet }) {
           >
             4708 Belwood green baltimore 21227
           </Text>
+        </View>
+
+        <View>
+          <Ionicons name="heart-outline" size={34} color="red" />
         </View>
       </View>
     </View>
